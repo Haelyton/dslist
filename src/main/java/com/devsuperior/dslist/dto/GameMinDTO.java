18d3034a04e39e3f226dcc262d0469274a159bda
MaiconDev <haelyton149@gmail.com>
2025-03-01 19:50:22 -0300

    Teste Homologação OK

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -53,7 +53,7 @@ public class GameMinDTO {
     public GameMinDTO(final GameMinProjection projection) {
         this.id = projection.getId();
         this.title = projection.getTitle();
-        this.year = projection.getYear();
+        this.year = projection.getGameYear();
         this.imgUrl = projection.getImgUrl();
         this.shortDescription = projection.getShortDescription();
     }
